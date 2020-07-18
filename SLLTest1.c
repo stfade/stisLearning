27ec9353fcd9e118df38bbc658e304bf6f4cbc42
stfade <samettaslioglu@gmail.com>
2020-07-19 01:57:34 +0300

    SLLDegistirme

---
+++ SLLTest1.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int num;                        
+    struct node *nextptr;           
+}*stnode;
+
+void createNodeList(int n); 
+void displayList();         
+struct node *search(int);
+void degistir(struct node *);
+void displayList2();
+int main()
+{
+    int n,a;
+    
+		printf("\n\n Linked List : To create and display Singly Linked List :\n");
+		printf("-------------------------------------------------------------\n");
+		
+    printf(" Input the number of nodes : ");
+    scanf("%d", &n);
+    createNodeList(n);
+    printf("\n Data entered in the list : \n");
+    displayList();
+    printf("\n aramak istediginiz sayiyi giriniz :\n");
+    scanf("%d",&a);
+    printf("\n aramak istediginiz sayi:%p\n",search(a));
+    struct node *temp=search(a);
+    degistir(temp);
+    displayList2();
+    return 0;
+}
+void createNodeList(int n)
+{
+    struct node *fnNode, *tmp;
+    int num, i;
+    stnode = (struct node *)malloc(sizeof(struct node));
+
+    if(stnode == NULL) 
+    {
+        printf(" Memory can not be allocated.");
+    }
+    else
+    {
+
+
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+        stnode->num = num;      
+        stnode->nextptr = NULL; 
+        tmp = stnode;
+
+        for(i=2; i<=n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL)
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+ 
+                fnNode->num = num;      
+                fnNode->nextptr = NULL; 
+ 
+                tmp->nextptr = fnNode; 
+                tmp = tmp->nextptr; 
+            }
+        }
+    }
+}
+void displayList()
+{
+    struct node *tmp;
+    if(stnode == NULL)
+    {
+        printf(" List is empty.");
+    }
+    else
+    {
+        tmp = stnode;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num); 
+            tmp = tmp->nextptr;                     
+        }
+    }
+}
+struct node *search(int a)
+{	
+	struct node *temp=NULL; 
+	temp = stnode;		
+	
+	while (temp != NULL)   
+	{
+	
+		if (a == temp->num) 	
+		{
+			return temp;
+		}
+		temp = temp->nextptr; 
+	}
+return NULL;
+}
+void degistir(struct node *temp)
+{
+	int x=0;
+	printf("yeni sayiyi yazin\n");
+	scanf("%d",&x);
+	temp->num =x;
+	printf("\nyeni sayi:%d\n",temp->num);
+}
+void displayList2()
+{
+    struct node *tmp;
+    if(stnode == NULL)
+    {
+        printf(" List is empty.");
+    }
+    else
+    {
+        tmp = stnode;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num); 
+            tmp = tmp->nextptr;                     
+        }
+    }
+}
