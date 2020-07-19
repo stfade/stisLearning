309b66bb62b2de50c3acb36df01011d3f11b0e3b
stfade <samettaslioglu@gmail.com>
2020-07-19 10:39:00 +0300

    SLLArayaEkle

--- SLLTest1.c
+++ SLLTest1.c
@@ -12,9 +12,11 @@ void displayList();
 struct node *search(int);
 void degistir(struct node *);
 void displayList2();
+void ekle(int,int);
+void displayList3();
 int main()
 {
-    int n,a;
+    int n,a,x,y;
     
 		printf("\n\n Linked List : To create and display Singly Linked List :\n");
 		printf("-------------------------------------------------------------\n");
@@ -26,10 +28,15 @@ int main()
     displayList();
     printf("\n aramak istediginiz sayiyi giriniz :\n");
     scanf("%d",&a);
-    printf("\n aramak istediginiz sayi:%p\n",search(a));
     struct node *temp=search(a);
     degistir(temp);
     displayList2();
+    printf("\n eklemek istediginiz sayiyi giriniz :\n");
+    scanf("%d",&x);
+    printf("\n hangi sayidan sonra eklenecek :\n");
+    scanf("%d",&y);
+    ekle(x,y);
+    displayList3();
     return 0;
 }
 void createNodeList(int n)
@@ -95,7 +102,6 @@ struct node *search(int a)
 {	
 	struct node *temp=NULL; 
 	temp = stnode;		
-	
 	while (temp != NULL)   
 	{
 	
@@ -116,6 +122,48 @@ void degistir(struct node *temp)
 	printf("\nyeni sayi:%d\n",temp->num);
 }
 void displayList2()
+{
+    struct node *tmp;
+    if(stnode == NULL)
+    {
+        printf(" List is empty.");
+    }
+    else
+    {
+        tmp = stnode;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num); 
+            tmp = tmp->nextptr;                     
+        }
+    }
+}
+void ekle(int t,int s)
+{
+	int i;
+	struct node *tutucu=NULL,*tmp=NULL,*gonderici=NULL;
+	tmp=stnode;
+	while (tmp != NULL)
+	{
+		if (s == tmp->num) 	
+		{
+			tutucu=tmp;
+			tutucu = (struct node *)malloc(sizeof(struct node));
+			if(tutucu == NULL) 
+    			{
+        			printf(" Memory can not be allocated.");
+    			}
+			tutucu->num =t;
+			tutucu->nextptr =tmp->nextptr;
+			tmp->nextptr =tutucu;
+			
+				
+		}
+		tmp = tmp->nextptr;  
+	}
+
+}
+void displayList3()
 {
     struct node *tmp;
     if(stnode == NULL)
