803e184440715ec119e95e30aa3de1085b513cd0
stfade <samettaslioglu@gmail.com>
2020-07-23 18:10:14 +0300

    List.c dosyasi

---
+++ List/list.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
+
+generic_list_t *generic_list_create()
+{
+	generic_list_t *this = malloc(sizeof(generic_list_t));
+
+	if (!this)
+	{
+		return NULL;
+	}
+
+	return this;
+}
+
+
+
+
+void generic_list_display(generic_list_t *this)
+{
+	struct generic_list *temp=NULL;
+	if(this == NULL)
+    	{
+        	printf(" List is empty.");
+    	}
+    	else
+	{
+        	temp = this;
+        	while(temp != NULL)
+        	{
+            		printf(" id = %d\n", temp->generic_variables.id); 
+            		temp = temp->next;                     
+        	}
+    }
+}
+
+
+
+void generic_list_add_node(generic_list_t *this, void *node)
+{
+	struct generic_list *temp=NULL;
+	this->generic_variables.id = 1; 
+	this->data = node;
+	this->next = NULL; 
+	temp = this;
+}
+
+
